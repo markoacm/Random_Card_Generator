6ac02862ce3444880e55a56f249b3fc09d4139d0
Marco_acm <71322098+markoacm@users.noreply.github.com>
2023-05-16 10:14:08 +0000

    Added style, index and app files to src folder

--- src/app.js
+++ src/app.js
@@ -2,10 +2,50 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById("change-card").onclick = function() {
+    const cardNumbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    const suitType = ["♦", "♥", "♠", "♣"];
+
+    let generateRandomNumber = () => {
+      let randomCardValue =
+        cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+      let randomSuit = suitType[Math.floor(Math.random() * suitType.length)];
+
+      let topSuit = document.querySelector(".top-suit");
+      topSuit.innerHTML = randomSuit;
+
+      let randomValue = document.querySelector(".number");
+      randomValue.innerHTML = randomCardValue;
+
+      let bottomSuit = document.querySelector(".bottom-suit");
+      bottomSuit.innerHTML = randomSuit;
+
+      if (randomSuit === "♥" || randomSuit === "♦") {
+        topSuit.classList.add("suit-color");
+        bottomSuit.classList.add("suit-color");
+      } else {
+        topSuit.classList.remove("suit-color");
+        bottomSuit.classList.remove("suit-color");
+      }
+    };
+
+    generateRandomNumber();
+  };
 };
